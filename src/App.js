8ce2276444d694353c65e797573ab9ca36e6b919
Keni Rios <kenirios11@egmail.com>
2022-01-14 10:57:21 -0500

    Subiendo Archivos NFT

---
+++ src/App.js
@@ -0,0 +1,384 @@
+/* eslint-disable no-undef */
+// Importaciones
+import React, {useEffect, useState } from "react";
+import './App.css';
+import {useDispatch, useSelector } from "react-redux";
+import { connect } from "./redux/blockchain/blockchainActions";
+import { fetchData } from "./redux/data/dataActions";
+import * as s from "./styles/globalStyles";
+import LipRenderer from "./comp/LipRenderer";
+import _color from "./assets/images/bg/_color.png";
+
+function App() {
+
+  // Primeros pasos en la carga de la información
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.data);
+  const [loading, setLoading] = useState(false);
+  const blockchain = useSelector((state) => state.blockchain);
+  console.log(data);
+ 
+
+
+  const  generateRandomString = (num) => {
+    //const characters ='ABECIDOFUGAHEJIKOLUMANEIPOQURASETIVOWUXAYEZI';
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result1= ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < num; i++ ) {
+        result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    
+    return result1;
+}
+
+
+  // Mint de un nuevo Token NFT
+  const mintNFT = async (_account, _name, cant) => {
+    setLoading(true);
+    blockchain.lipToken.methods.createRandomLip(_name, cant).send({
+      from: _account,
+      value: blockchain.web3.utils.toWei("1", "ether"),
+    }).once("error", (err) => {
+      setLoading(false);
+      console.log(err);
+    }).then((receipt) => {
+      setLoading(false);
+      console.log(receipt);
+      dispatch(fetchData(blockchain.account));
+    });
+  };
+
+
+  const mintNFT2 = async (_account, _name, cant) => {
+    setLoading(true);
+    blockchain.lipToken.methods.createRandomLip(_name, cant).send({
+      from: _account,
+      value: blockchain.web3.utils.toWei("2", "ether"),
+    }).once("error", (err) => {
+      setLoading(false);
+      console.log(err);
+    }).then((receipt) => {
+      setLoading(false);
+      console.log(receipt);
+      dispatch(fetchData(blockchain.account));
+    });
+  };
+
+  const mintNFT3 = async (_account, _name, cant) => {
+    setLoading(true);
+    blockchain.lipToken.methods.createRandomLip(_name, cant).send({
+      from: _account,
+      value: blockchain.web3.utils.toWei("3", "ether"),
+    }).once("error", (err) => {
+      setLoading(false);
+      console.log(err);
+    }).then((receipt) => {
+      setLoading(false);
+      console.log(receipt);
+      dispatch(fetchData(blockchain.account));
+    });
+  };
+
+
+  // Subir nivel de un Token NFT
+  const levelUpLip = async (_account, _id) => {
+    setLoading(true);
+    blockchain.lipToken.methods.levelUp(_id).send({     
+      value: blockchain.web3.utils.toWei("0.2", "ether"),
+      from: _account,
+    }).once("error", (err) => {
+      setLoading(false);
+      console.log(err);
+    }).then((receipt) => {
+      setLoading(false);
+      console.log(receipt);
+      dispatch(fetchData(blockchain.account));
+    });
+  };
+
+  // Subir vida de un Token NFT
+  const vidaUpLip = async (_account, _id) => {
+    setLoading(true);
+    blockchain.lipToken.methods.vidaUp(_id).send({     
+      value: blockchain.web3.utils.toWei("0.3", "ether"),
+      from: _account,
+    }).once("error", (err) => {
+      setLoading(false);
+      console.log(err);
+    }).then((receipt) => {
+      setLoading(false);
+      console.log(receipt);
+      dispatch(fetchData(blockchain.account));
+    });
+  };
+
+///////////////////////  habilidad fuerza defenza y poder de pelea
+
+
+// Subir habilidad de un Token NFT
+const habilUpLip = async (_account, _id) => {
+  setLoading(true);
+  blockchain.lipToken.methods.habilUp(_id).send({     
+    value: blockchain.web3.utils.toWei("0.1", "ether"),
+    from: _account,
+  }).once("error", (err) => {
+    setLoading(false);
+    console.log(err);
+  }).then((receipt) => {
+    setLoading(false);
+    console.log(receipt);
+    dispatch(fetchData(blockchain.account));
+  });
+};
+
+// Subir defenza de un Token NFT
+const defenzaUpLip = async (_account, _id) => {
+  setLoading(true);
+  blockchain.lipToken.methods.defenzaUp(_id).send({     
+    value: blockchain.web3.utils.toWei("0.2", "ether"),
+    from: _account,
+  }).once("error", (err) => {
+    setLoading(false);
+    console.log(err);
+  }).then((receipt) => {
+    setLoading(false);
+    console.log(receipt);
+    dispatch(fetchData(blockchain.account));
+  });
+};
+
+
+// Subir fuerza de un Token NFT
+const fuerzaUpLip = async (_account, _id) => {
+  setLoading(true);
+  blockchain.lipToken.methods.fuerzaUp(_id).send({     
+    value: blockchain.web3.utils.toWei("0.5", "ether"),
+    from: _account,
+  }).once("error", (err) => {
+    setLoading(false);
+    console.log(err);
+  }).then((receipt) => {
+    setLoading(false);
+    console.log(receipt);
+    dispatch(fetchData(blockchain.account));
+  });
+};
+
+
+// Subir poder de un Token NFT
+const poderUpLip = async (_account, _id) => {
+  setLoading(true);
+  blockchain.lipToken.methods.poderUp(_id).send({     
+    value: blockchain.web3.utils.toWei("0.5", "ether"),
+    from: _account,
+  }).once("error", (err) => {
+    setLoading(false);
+    console.log(err);
+  }).then((receipt) => {
+    setLoading(false);
+    console.log(receipt);
+    dispatch(fetchData(blockchain.account));
+  });
+};
+
+///////////////////////  habilidad fuerza defenza y poder de pelea
+
+
+
+
+  // Visualizar el balance del Smart Contract
+  const balanceSmartContract = async () => {
+    setLoading(true);
+    const money = blockchain.lipToken.methods.moneySmartContract().call();
+    money.then(value => {
+      alert(parseFloat(value/1000000000000000000))})
+      console.log(money)
+  };
+
+  // Obtención del dinero por el Owner del Smart Contract
+  const ethersOwner = async (_account) => {
+    setLoading(true);
+    blockchain.lipToken.methods.withdraw().send({
+      from: _account,
+    }).once("error", (err) => {
+      setLoading(false);
+      console.log(err);
+    }).then((receipt) => {
+      setLoading(false);
+      console.log(receipt);
+      dispatch(fetchData(blockchain.account));
+    });
+  };
+
+  // Recurso: https://es.reactjs.org/docs/hooks-effect.html
+  // De forma similar a componentDidMount y componentDidUpdate
+  useEffect (() => {
+    if(blockchain.account != "" && blockchain.lipToken != null){
+      dispatch(fetchData(blockchain.account));
+    }
+  }, [blockchain.lipToken]);
+  
+
+  // Visualización del Videojuego NFT 
+  return (
+    <s.Screen image={_color}>
+      {blockchain.account === "" || blockchain.lipToken === null ? (
+        <s.Container flex = {1} ai = {"center"} jc = {"center"}>
+          <s.TextTitle> ¡AniMons Game, Juego NFT de Batallas Epicas!</s.TextTitle>
+          <s.SpacerSmall/>
+        <s.Button1 
+          onClick = {(e) => {
+          e.preventDefault();
+          dispatch(connect());
+        }}> 
+        CONECTAR  
+        </s.Button1>
+        <s.SpacerSmall/>
+        {blockchain.errorMsg != "" ? ( 
+          <s.TextDescription>{blockchain.errorMsg}</s.TextDescription> ) : null}
+        </s.Container> ) : (
+          <s.Container ai = {"center"} style = {{padding: "24px"}}>
+          <s.TextTitle> ¡Bienvenido al Videojuego NFT! </s.TextTitle>
+          <s.SpacerSmall/>
+          
+          <s.Button2 
+          
+           onClick = {(e) => {
+             e.preventDefault();
+             const name = " AniMons-"+generateRandomString(4);
+             const cant = '2';
+             mintNFT(blockchain.account, name, cant)
+           }} > 
+           Crear Nuevo Pack de 3 NFT 
+           </s.Button2>
+
+                  
+          <s.Button2 
+           onClick = {(e) => {
+             e.preventDefault();
+             const name = " AniMons-"+generateRandomString(4) 
+             const cant = '3';
+             mintNFT2(blockchain.account, name, cant)
+           }} > 
+           Crear Nuevo Pack de 4 NFT 
+           </s.Button2>
+                 
+          <s.Button2 
+           onClick = {(e) => {
+             e.preventDefault();
+             const name = " AniMons-"+generateRandomString(4) 
+             const cant = '4';
+             mintNFT3(blockchain.account, name, cant)
+           }} > 
+          Crear Nuevo Pack de 5 NFT 
+           </s.Button2>
+          
+
+
+
+          <s.Container jc = {"center"} fd = {"row"} style = {{flexWrap: "wrap"}} >
+            {data.allLips.map((item, index) => {
+              
+              return ( 
+               
+                <s.Container key = {index} style = {{padding: "15px"}} >
+                  <LipRenderer lip = {item} />
+                  <s.SpacerXSmall/>
+                  <s.Container>               
+                  <s.TextDescription>{item.name}</s.TextDescription>                                                      
+                    <s.TextDescription>Id: {item.id}</s.TextDescription>                      
+                    <s.TextDescription>Vida: {item.vida}</s.TextDescription>                
+                    <s.TextDescription>Nivel: {item.level}</s.TextDescription>
+                    <s.TextDescription>Fuerza: {item.fuerza}</s.TextDescription>                    
+                    <s.TextDescription>Rareza: {item.rarity}</s.TextDescription>                    
+                    <s.TextDescription>Defenza: {item.defenza}</s.TextDescription>
+                    <s.TextDescription>Habilidad: {item.habil}</s.TextDescription>                                      
+                    <s.TextDescription>Poder de Pelea: {item.poder}</s.TextDescription>
+                    <s.TextDescription>ADN: {item.dna}</s.TextDescription>                            
+                    <s.SpacerXSmall/>
+
+                    <s.Button3
+                    disabled = {loading ? 1:0}
+                    onClick = {(e) => {
+                      e.preventDefault();
+                      levelUpLip(blockchain.account, item.id);
+                    }}>
+                      Pack 1 Nivel
+                    </s.Button3>
+
+                    <s.Button3
+                    disabled = {loading ? 1:0}
+                    onClick = {(e) => {
+                      e.preventDefault();
+                      vidaUpLip(blockchain.account, item.id);
+                    }}>
+                      15 Vidas
+                    </s.Button3>
+
+                    <s.Button3
+                    disabled = {loading ? 1:0}
+                    onClick = {(e) => {
+                      e.preventDefault();
+                      habilUpLip(blockchain.account, item.id);
+                    }}>
+                      3 Habilidad
+                    </s.Button3>                    
+
+                    <s.Button3
+                    disabled = {loading ? 1:0}
+                    onClick = {(e) => {
+                      e.preventDefault();
+                      fuerzaUpLip(blockchain.account, item.id);
+                    }}>
+                      5 de fuerza
+                    </s.Button3>
+
+
+                    <s.Button3
+                    disabled = {loading ? 1:0}
+                    onClick = {(e) => {
+                      e.preventDefault();
+                      defenzaUpLip(blockchain.account, item.id);
+                    }}>
+                      3 Defenza
+                    </s.Button3>
+
+                    <s.Button3
+                    disabled = {loading ? 1:0}
+                    onClick = {(e) => {
+                      e.preventDefault();
+                      poderUpLip(blockchain.account, item.id);
+                    }}>
+                      10 P Pelea
+                    </s.Button3>
+
+                  </s.Container>
+                  </s.Container>
+              );
+            })}
+          </s.Container>
+          
+          <s.Button4
+            onClick = {(e) => {
+              e.preventDefault();
+              balanceSmartContract();
+            }}>
+              BALANCE DEL SMART CONTRACT
+            </s.Button4>
+
+          <s.Button4
+            onClick = {(e) => {
+              e.preventDefault();
+              ethersOwner(blockchain.account);
+            }}>
+              RETIRAR DINERO
+            </s.Button4>
+            </s.Container>
+        )}
+    </s.Screen> 
+  );
+}
+
+
+export default App;
---
+++ src/parts/parts.js
@@ -0,0 +1,59 @@
+// Llamada a las imágenes del Token NFT
+import a_0 from "../assets/images/parts/a/0.png"
+import a_1 from "../assets/images/parts/a/1.png"
+import a_2 from "../assets/images/parts/a/2.png"
+import a_3 from "../assets/images/parts/a/3.png"
+import a_4 from "../assets/images/parts/a/4.png"
+import a_5 from "../assets/images/parts/a/5.png"
+
+import b_0 from "../assets/images/parts/b/0.png"
+import b_1 from "../assets/images/parts/b/1.png"
+import b_2 from "../assets/images/parts/b/2.png"
+import b_3 from "../assets/images/parts/b/3.png"
+import b_4 from "../assets/images/parts/b/4.png"
+import b_5 from "../assets/images/parts/b/5.png"
+
+import c_0 from "../assets/images/parts/c/0.png"
+import c_1 from "../assets/images/parts/c/1.png"
+import c_2 from "../assets/images/parts/c/2.png"
+import c_3 from "../assets/images/parts/c/3.png"
+import c_4 from "../assets/images/parts/c/4.png"
+import c_5 from "../assets/images/parts/c/5.png"
+
+import d_0 from "../assets/images/parts/d/0.png"
+import d_1 from "../assets/images/parts/d/1.png"
+import d_2 from "../assets/images/parts/d/2.png"
+import d_3 from "../assets/images/parts/d/3.png"
+import d_4 from "../assets/images/parts/d/4.png"
+import d_5 from "../assets/images/parts/d/5.png"
+
+import e_0 from "../assets/images/parts/e/0.png"
+import e_1 from "../assets/images/parts/e/1.png"
+import e_2 from "../assets/images/parts/e/2.png"
+import e_3 from "../assets/images/parts/e/3.png"
+import e_4 from "../assets/images/parts/e/4.png"
+import e_5 from "../assets/images/parts/e/5.png"
+
+import f_0 from "../assets/images/parts/f/0.png"
+import f_1 from "../assets/images/parts/f/1.png"
+import f_2 from "../assets/images/parts/f/2.png"
+import f_3 from "../assets/images/parts/f/3.png"
+import f_4 from "../assets/images/parts/f/4.png"
+import f_5 from "../assets/images/parts/f/5.png"
+
+import g_0 from "../assets/images/parts/g/0.png"
+import g_1 from "../assets/images/parts/g/1.png"
+import g_2 from "../assets/images/parts/g/2.png"
+import g_3 from "../assets/images/parts/g/3.png"
+import g_4 from "../assets/images/parts/g/4.png"
+import g_5 from "../assets/images/parts/g/5.png"
+
+export const parts = {
+    bg: [a_0, a_1, a_2, a_3, a_4, a_5],
+    mask: [b_0, b_1, b_2, b_3, b_4, b_5 ],
+    line: [c_0, c_1, c_2, c_3, c_4, c_5],
+    addon: [d_0, d_1, d_2, d_3, d_4, d_5],
+    addonMouth1: [e_0, e_1, e_2, e_3, e_4, e_5],
+    addonMouth2: [f_0, f_1, f_2, f_3, f_4, f_5],
+    addonMouth3: [g_0, g_1, g_2, g_3, g_4, g_5],
+}
